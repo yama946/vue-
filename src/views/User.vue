24d7fccc06c3a6b95c82d24e60ac43c3db7111a3
yama946 <code_yama_other@163.com>
2022-10-27 07:52:43 +0800

    表单校验功能

--- src/views/User.vue
+++ src/views/User.vue
@@ -6,23 +6,23 @@
 
     <el-dialog title="新增用户" :visible.sync="dialogVisible" width="50%" :before-close="handleClose">
         <!-- 用户的表单数据 -->
-        <el-form :inline="true" :label-position="labelPosition" ref="form" :model="form" label-width="80px">
-            <el-form-item label="姓名">
+        <el-form :inline="true" :rules="rules" :label-position="labelPosition" ref="form" :model="form" label-width="80px">
+            <el-form-item label="姓名" prop="name">
                 <el-input placeholder="请输入姓名" v-model="form.name"></el-input>
             </el-form-item>
-            <el-form-item label="年龄">
+            <el-form-item label="年龄" prop="age">
                 <el-input placeholder="请输入年龄" v-model="form.age"></el-input>
             </el-form-item>
-            <el-form-item label="性别">
+            <el-form-item label="性别" prop="sex">
                 <el-select v-model="form.sex" placeholder="请选择性别">
                     <el-option label="男" value="1"></el-option>
                     <el-option label="女" value="0"></el-option>
                 </el-select>
             </el-form-item>
-            <el-form-item label="出生日期">
+            <el-form-item label="出生日期" prop="birth">
                 <el-date-picker type="date" placeholder="选择日期" v-model="form.birth" style="width: 100%;"></el-date-picker>
             </el-form-item>
-            <el-form-item label="地址">
+            <el-form-item label="地址" prop="addr">
                 <el-input placeholder="请输入地址" v-model="form.addr"></el-input>
             </el-form-item>
         </el-form>
@@ -54,6 +54,26 @@ export default {
                 sex: '', //性别
                 birth: '', //生日
                 addr: '', //地址
+            },
+            //表单校验
+            rules: {
+                name: [
+                    { required: true, message: '请输入姓名', trigger: 'blur' },
+                    { min: 2, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+                ],
+                age: [
+                    { required: true, message: '请输入年龄', trigger: 'blur' },
+                    { min: 1, max: 3, message: '数据非法,请重新填写', trigger: 'blur' }
+                ],
+                sex :[
+                    { required: true, message: '请选择性别', trigger: 'blur' },
+                ],
+                birth: [
+                    { required: true, message: '请选择出生日期', trigger: 'blur' },
+                ],
+                addr: [
+                    { required: true, message: '请输入地址', trigger: 'blur' },
+                ]
             }
         };
     },
